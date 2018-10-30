0ed33794348e6788264999822cd0c17b444b3777
Cavicchioli Michael <michaelcavicchioli.informatica@gmail.com>
2018-10-30 11:06:57 +0100

    Primo commit

---
+++ EsercizioGit/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace EsercizioGit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbMassimo = new System.Windows.Forms.TextBox();
+            this.txtbMinimo = new System.Windows.Forms.TextBox();
+            this.lbMassimo = new System.Windows.Forms.Label();
+            this.lbMinimo = new System.Windows.Forms.Label();
+            this.lbArray = new System.Windows.Forms.Label();
+            this.txtb1 = new System.Windows.Forms.TextBox();
+            this.txtb2 = new System.Windows.Forms.TextBox();
+            this.txtb3 = new System.Windows.Forms.TextBox();
+            this.txtb4 = new System.Windows.Forms.TextBox();
+            this.txtb5 = new System.Windows.Forms.TextBox();
+            this.txtb6 = new System.Windows.Forms.TextBox();
+            this.txtb7 = new System.Windows.Forms.TextBox();
+            this.txtb8 = new System.Windows.Forms.TextBox();
+            this.txtb9 = new System.Windows.Forms.TextBox();
+            this.txtb10 = new System.Windows.Forms.TextBox();
+            this.bGeneraArray = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtbMassimo
+            // 
+            this.txtbMassimo.Location = new System.Drawing.Point(79, 91);
+            this.txtbMassimo.Name = "txtbMassimo";
+            this.txtbMassimo.Size = new System.Drawing.Size(76, 20);
+            this.txtbMassimo.TabIndex = 0;
+            // 
+            // txtbMinimo
+            // 
+            this.txtbMinimo.Location = new System.Drawing.Point(79, 127);
+            this.txtbMinimo.Name = "txtbMinimo";
+            this.txtbMinimo.Size = new System.Drawing.Size(76, 20);
+            this.txtbMinimo.TabIndex = 1;
+            // 
+            // lbMassimo
+            // 
+            this.lbMassimo.AutoSize = true;
+            this.lbMassimo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMassimo.ForeColor = System.Drawing.Color.White;
+            this.lbMassimo.Location = new System.Drawing.Point(18, 94);
+            this.lbMassimo.Name = "lbMassimo";
+            this.lbMassimo.Size = new System.Drawing.Size(55, 13);
+            this.lbMassimo.TabIndex = 2;
+            this.lbMassimo.Text = "Massimo";
+            // 
+            // lbMinimo
+            // 
+            this.lbMinimo.AutoSize = true;
+            this.lbMinimo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMinimo.ForeColor = System.Drawing.Color.White;
+            this.lbMinimo.Location = new System.Drawing.Point(19, 130);
+            this.lbMinimo.Name = "lbMinimo";
+            this.lbMinimo.Size = new System.Drawing.Size(46, 13);
+            this.lbMinimo.TabIndex = 3;
+            this.lbMinimo.Text = "Minimo";
+            this.lbMinimo.Click += new System.EventHandler(this.lbMinimo_Click);
+            // 
+            // lbArray
+            // 
+            this.lbArray.AutoSize = true;
+            this.lbArray.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbArray.ForeColor = System.Drawing.Color.White;
+            this.lbArray.Location = new System.Drawing.Point(19, 44);
+            this.lbArray.Name = "lbArray";
+            this.lbArray.Size = new System.Drawing.Size(36, 13);
+            this.lbArray.TabIndex = 4;
+            this.lbArray.Text = "Array";
+            // 
+            // txtb1
+            // 
+            this.txtb1.Location = new System.Drawing.Point(61, 41);
+            this.txtb1.Name = "txtb1";
+            this.txtb1.Size = new System.Drawing.Size(18, 20);
+            this.txtb1.TabIndex = 5;
+            // 
+            // txtb2
+            // 
+            this.txtb2.Location = new System.Drawing.Point(85, 41);
+            this.txtb2.Name = "txtb2";
+            this.txtb2.Size = new System.Drawing.Size(18, 20);
+            this.txtb2.TabIndex = 6;
+            // 
+            // txtb3
+            // 
+            this.txtb3.Location = new System.Drawing.Point(109, 41);
+            this.txtb3.Name = "txtb3";
+            this.txtb3.Size = new System.Drawing.Size(18, 20);
+            this.txtb3.TabIndex = 7;
+            // 
+            // txtb4
+            // 
+            this.txtb4.Location = new System.Drawing.Point(133, 41);
+            this.txtb4.Name = "txtb4";
+            this.txtb4.Size = new System.Drawing.Size(18, 20);
+            this.txtb4.TabIndex = 8;
+            // 
+            // txtb5
+            // 
+            this.txtb5.Location = new System.Drawing.Point(157, 41);
+            this.txtb5.Name = "txtb5";
+            this.txtb5.Size = new System.Drawing.Size(18, 20);
+            this.txtb5.TabIndex = 9;
+            // 
+            // txtb6
+            // 
+            this.txtb6.Location = new System.Drawing.Point(181, 41);
+            this.txtb6.Name = "txtb6";
+            this.txtb6.Size = new System.Drawing.Size(18, 20);
+            this.txtb6.TabIndex = 10;
+            // 
+            // txtb7
+            // 
+            this.txtb7.Location = new System.Drawing.Point(205, 41);
+            this.txtb7.Name = "txtb7";
+            this.txtb7.Size = new System.Drawing.Size(18, 20);
+            this.txtb7.TabIndex = 11;
+            // 
+            // txtb8
+            // 
+            this.txtb8.Location = new System.Drawing.Point(229, 41);
+            this.txtb8.Name = "txtb8";
+            this.txtb8.Size = new System.Drawing.Size(18, 20);
+            this.txtb8.TabIndex = 12;
+            // 
+            // txtb9
+            // 
+            this.txtb9.Location = new System.Drawing.Point(253, 41);
+            this.txtb9.Name = "txtb9";
+            this.txtb9.Size = new System.Drawing.Size(18, 20);
+            this.txtb9.TabIndex = 13;
+            // 
+            // txtb10
+            // 
+            this.txtb10.Location = new System.Drawing.Point(277, 41);
+            this.txtb10.Name = "txtb10";
+            this.txtb10.Size = new System.Drawing.Size(18, 20);
+            this.txtb10.TabIndex = 14;
+            // 
+            // bGeneraArray
+            // 
+            this.bGeneraArray.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bGeneraArray.Location = new System.Drawing.Point(181, 81);
+            this.bGeneraArray.Name = "bGeneraArray";
+            this.bGeneraArray.Size = new System.Drawing.Size(113, 23);
+            this.bGeneraArray.TabIndex = 15;
+            this.bGeneraArray.Text = "Genera Array";
+            this.bGeneraArray.UseVisualStyleBackColor = true;
+            this.bGeneraArray.Click += new System.EventHandler(this.bGeneraArray_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(182, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Calcolo max/min";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(312, 179);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.bGeneraArray);
+            this.Controls.Add(this.txtb10);
+            this.Controls.Add(this.txtb9);
+            this.Controls.Add(this.txtb8);
+            this.Controls.Add(this.txtb7);
+            this.Controls.Add(this.txtb6);
+            this.Controls.Add(this.txtb5);
+            this.Controls.Add(this.txtb4);
+            this.Controls.Add(this.txtb3);
+            this.Controls.Add(this.txtb2);
+            this.Controls.Add(this.txtb1);
+            this.Controls.Add(this.lbArray);
+            this.Controls.Add(this.lbMinimo);
+            this.Controls.Add(this.lbMassimo);
+            this.Controls.Add(this.txtbMinimo);
+            this.Controls.Add(this.txtbMassimo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbMassimo;
+        private System.Windows.Forms.TextBox txtbMinimo;
+        private System.Windows.Forms.Label lbMassimo;
+        private System.Windows.Forms.Label lbMinimo;
+        private System.Windows.Forms.Label lbArray;
+        private System.Windows.Forms.TextBox txtb1;
+        private System.Windows.Forms.TextBox txtb2;
+        private System.Windows.Forms.TextBox txtb3;
+        private System.Windows.Forms.TextBox txtb4;
+        private System.Windows.Forms.TextBox txtb5;
+        private System.Windows.Forms.TextBox txtb6;
+        private System.Windows.Forms.TextBox txtb7;
+        private System.Windows.Forms.TextBox txtb8;
+        private System.Windows.Forms.TextBox txtb9;
+        private System.Windows.Forms.TextBox txtb10;
+        private System.Windows.Forms.Button bGeneraArray;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ EsercizioGit/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EsercizioGit
+{
+
+    public partial class Form1 : Form
+    {
+        int[] V = new int[10];
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbMinimo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bGeneraArray_Click(object sender, EventArgs e)
+        {
+            for (int i=0; i < 10; i++)
+            {
+                V[i] = r.Next(50);
+            }
+            txtb1.Text = V[0].ToString();
+            txtb2.Text = V[1].ToString();
+            txtb3.Text = V[2].ToString();
+            txtb4.Text = V[3].ToString();
+            txtb5.Text = V[4].ToString();
+            txtb6.Text = V[5].ToString();
+            txtb7.Text = V[6].ToString();
+            txtb8.Text = V[7].ToString();
+            txtb9.Text = V[8].ToString();
+            txtb10.Text = V[9].ToString();
+            txtbMassimo.Text = "";
+            txtbMinimo.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtbMassimo.Text = V.Max().ToString();
+            txtbMinimo.Text = V.Min().ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bOrdinamento_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
